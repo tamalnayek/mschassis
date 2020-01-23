b3b131a09413634f4896a03b7d22c75fc01597ae
w2kube01 <w2kube01@iiht.tech>
2020-01-23 16:39:24 +0530

    chassis update

--- stopchassis.bat
+++ stopchassis.bat
@@ -2,22 +2,22 @@ echo off
 echo "Stopping consul.........."
 docker-compose -f consul/docker-compose.yaml down
 
-echo "Stopping consul.........."
-docker-compose -f consul/docker-compose.yaml down
-
-echo "Stopping vault.........."
-docker-compose -f vaultroot/docker-compose.yaml down
-
 echo "Stopping zipkin.........."
 docker-compose -f zipkin/docker-compose.yaml down
 
+echo "Stopping jenkins.........."
+docker-compose -f jenkins/docker-compose.yaml down
+
+echo "Stopping prometheus.........."
 docker-compose -f prometheus/docker-compose.yaml down
 
 echo "Stopping mongo DB.........."
 docker-compose -f mongodb/docker-compose.yaml down
 
-echo "Stopping jenkins.........."
-docker-compose -f jenkins/docker-compose.yaml down
+echo "Stopping vault.........."
+docker-compose -f vaultroot/docker-compose.yaml down
+
+
 
 echo "Stopping chassis network.........."
 docker network rm ms-chassis-nw
